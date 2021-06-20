b8dc53ee641b02cebd5ced700e73b0a3aa7f9b43
Pavel Biychuk <paul.biychuk@yandex.by>
2021-06-20 16:22:30 +0300

    hw_09

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -84,7 +84,6 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
