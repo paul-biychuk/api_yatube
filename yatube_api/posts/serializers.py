d63ec239af62889112eacceeb8bc8b98b3cfccd4
Pavel Biychuk <paul.biychuk@yandex.by>
2021-06-25 22:02:46 +0300

    corrected hw_09

--- yatube_api/posts/serializers.py
+++ yatube_api/posts/serializers.py
@@ -1,32 +1,31 @@
-from rest_framework import serializers 
-from .models import Post, Comment 
- 
- 
-class PostSerializer(serializers.ModelSerializer): 
- 
+from rest_framework import serializers
+from .models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
 
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True,
-        ) 
- 
-    class Meta: 
-        fields = ('author', 'pub_date', 'id', 'text') 
-        read_only_fields = ('pub_date',) 
-        model = Post 
- 
- 
-class CommentSerializer(serializers.ModelSerializer): 
-    author = serializers.SlugRelatedField( 
-        slug_field='username', 
-        read_only=True, 
-    ) 
- 
-    post = serializers.SlugRelatedField( 
-        slug_field='id', 
-        read_only=True, 
-    ) 
- 
-    class Meta: 
-        fields = ('id', 'post', 'author', 'text', 'created') 
-        model = Comment 
+    )
+
+    class Meta:
+        fields = ('author', 'pub_date', 'id', 'text')
+        read_only_fields = ('pub_date',)
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    post = serializers.SlugRelatedField(
+        slug_field='id',
+        read_only=True,
+    )
+
+    class Meta:
+        fields = ('id', 'post', 'author', 'text', 'created')
+        model = Comment
--- yatube_api/posts/urls.py
+++ yatube_api/posts/urls.py
@@ -1,19 +1,19 @@
-from rest_framework.authtoken import views 
-from .views import CommentViewSet, PostViewSet 
-from django.urls import include, path 
-from rest_framework.routers import DefaultRouter 
- 
- 
-router = DefaultRouter() 
-router.register( 
+from rest_framework.authtoken import views
+from .views import CommentViewSet, PostViewSet
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-    'posts/(?P<id>[0-9]+)/comments', CommentViewSet, basename='Comment' 
-) 
-router.register('posts', PostViewSet) 
- 
 
-urlpatterns = [ 
+router = DefaultRouter()
+router.register(
 
-    path('v1/api-token-auth/', views.obtain_auth_token), 
-    path('v1/', include(router.urls)), 
+    'posts/(?P<id>[0-9]+)/comments', CommentViewSet, basename='Comment'
+)
+router.register('posts', PostViewSet)
+
+
+urlpatterns = [
+
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router.urls)),
 ]
