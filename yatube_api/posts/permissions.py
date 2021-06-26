5caf90dbf4d4660b26767457fae4f070a2b8d3b1
Pavel Biychuk <paul.biychuk@yandex.by>
2021-06-26 17:20:20 +0300

    hw_09

--- yatube_api/posts/permissions.py
+++ yatube_api/posts/permissions.py
@@ -2,8 +2,6 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    # Не убирал IsAuthorOrReadOnly, т.к. там была пометка можно лучше,
-    #  чтобы удостовериться, что правильно сделал.
 
     def has_object_permission(self, request, view, obj):
 
--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -19,23 +19,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
         serializer.save(author=self.request.user)
 
-    def partial_update(self, request, pk, partial=True):
-
-        post = self.get_queryset().get(pk=pk)
-        serializer = self.get_serializer(post, data=request.data, partial=True)
-        if serializer.is_valid() and post.author == self.request.user:
-            serializer.save(author=self.request.user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
-
-    def destroy(self, request, pk):
-
-        post = self.get_queryset().get(pk=pk)
-        if post.author != self.request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        self.perform_destroy(post)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 class CommentViewSet(viewsets.ModelViewSet):
 
@@ -53,22 +36,4 @@ class CommentViewSet(viewsets.ModelViewSet):
 
         post = get_object_or_404(Post, id=self.kwargs['id'])
         serializer.save(post=post, author=self.request.user)
-
-    def partial_update(self, request, id, pk, partial=True):
-
-        comment = get_object_or_404(self.get_queryset(), pk=pk)
-        serializer = self.get_serializer(
-            comment, data=request.data, partial=True)
-        # Оставил author == self.request.user, т.к. не проходят тесты.
-        if serializer.is_valid() and comment.author == self.request.user:
-            serializer.save(author=self.request.user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
-
-    def destroy(self, request, id, pk):
-
-        comment = get_object_or_404(self.get_queryset(), pk=pk)
-        if comment.author != self.request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        self.perform_destroy(comment)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# Теперь понял) Спасибо!
