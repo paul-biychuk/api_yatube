f539b9be9b7d058cc4e925c715ef031f8b551c0f
Pavel Biychuk <paul.biychuk@yandex.by>
2021-06-26 17:27:17 +0300

    hw_09

--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import  viewsets, permissions
+from rest_framework import viewsets, permissions
 from .permissions import IsAuthorOrReadOnly
 
 from .models import Post
